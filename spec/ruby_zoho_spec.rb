59ab6cd36bc354d9d24c096f1e0ca78340c8fd61
amalc <amalc@pobox.com>
2013-02-22 02:56:33 -0500

    Update docs.
    [ci skip]

--- spec/ruby_zoho_spec.rb
+++ spec/ruby_zoho_spec.rb
@@ -9,7 +9,7 @@ describe RubyZoho::Crm do
     base_path = File.join(File.dirname(__FILE__), 'fixtures')
     @sample_pdf = File.join(base_path, 'sample.pdf')
     RubyZoho.configure do |config|
-      config.api_key = ENV['ZOHO_AP_KEY']
+      config.api_key = ENV['ZOHO_API_KEY']
       config.crm_modules = %w(Quotes)
       config.cache_fields = true
     end
