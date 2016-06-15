e325a8ce12dd9760fd77bf0b9ae7b19774d3c59b
Ben Knowles <benknowles@users.noreply.github.com>
2016-06-15 15:45:07 -0500

    Add configuration option for cache file path

--- lib/ruby_zoho.rb
+++ lib/ruby_zoho.rb
@@ -5,12 +5,13 @@ require 'yaml'
 module RubyZoho
 
   class Configuration
-    attr_accessor :api, :api_key, :cache_fields, :crm_modules, :ignore_fields_with_bad_names
+    attr_accessor :api, :api_key, :cache_fields, :cache_path, :crm_modules, :ignore_fields_with_bad_names
 
     def initialize
       self.api_key = nil
       self.api = nil
       self.cache_fields = false
+      self.cache_path = File.join(File.dirname(__FILE__), '..', 'spec', 'fixtures')
       self.crm_modules = nil
       self.ignore_fields_with_bad_names = true
     end
@@ -27,20 +28,20 @@ module RubyZoho
     self.configuration.crm_modules = %w[Accounts Calls Contacts Events Leads Potentials Tasks].concat(
         self.configuration.crm_modules).uniq
     self.configuration.api = init_api(self.configuration.api_key,
-                                      self.configuration.crm_modules, self.configuration.cache_fields)
+                                      self.configuration.crm_modules,
+                                      self.configuration.cache_fields, self.configuration.cache_path)
     RubyZoho::Crm.setup_classes()
   end
 
-  def self.init_api(api_key, modules, cache_fields)
-    base_path = File.join(File.dirname(__FILE__), '..', 'spec', 'fixtures')
-    if File.exists?(File.join(base_path, 'fields.snapshot')) && cache_fields == true
-      fields = YAML.load(File.read(File.join(base_path, 'fields.snapshot')))
+  def self.init_api(api_key, modules, cache_fields, cache_path)
+    if File.exists?(File.join(cache_path, 'fields.snapshot')) && cache_fields == true
+      fields = YAML.load(File.read(File.join(cache_path, 'fields.snapshot')))
       zoho = ZohoApi::Crm.new(api_key, modules,
                               self.configuration.ignore_fields_with_bad_names, fields)
     else
       zoho = ZohoApi::Crm.new(api_key, modules, self.configuration.ignore_fields_with_bad_names)
       fields = zoho.module_fields
-      File.open(File.join(base_path, 'fields.snapshot'), 'wb') { |file| file.write(fields.to_yaml) } if cache_fields == true
+      File.open(File.join(cache_path, 'fields.snapshot'), 'wb') { |file| file.write(fields.to_yaml) } if cache_fields == true
     end
     zoho
   end
